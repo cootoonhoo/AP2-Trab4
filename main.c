e19e8884eafb24515b42193f3ac9d9470dc1aa45
Marco Antonio <cootoonhoo@gmail.com>
2023-11-03 16:31:57 -0300

    Fazendo a lógica para leitura dos arquivos

--- main.c
+++ main.c
@@ -12,24 +12,65 @@ void GerarNomeArquivo(int numArq, char * nomeArquivo)
     strcat(nomeArquivo,".bin");
 }
 
+int LeturaArquivo(FILE * arquivo, char * maiorPalavra, char * menorPalavra)
+{
+    char caractere = 0, palavraLida[255];
+    palavraLida[0]='\0';
+    int qntCarac = 0, tamMaiorPalavra = 0, tamMenorPalavra = sizeof(int)*2, qntPalavras = 0;
+
+
+    while ((caractere = fgetc(arquivo)) != EOF)
+    {
+        if((caractere >= 'A' && caractere <='Z') || (caractere >='a' && caractere <='z'))
+        {
+            palavraLida[qntCarac] = caractere;
+            qntCarac++;
+        }
+        if(caractere == 32 || caractere == 46)
+        {
+            palavraLida[qntCarac] = '\0';
+            if(qntCarac > tamMaiorPalavra && qntCarac > 0)
+            {
+                maiorPalavra[0]='\0';
+                tamMaiorPalavra = qntCarac;
+                strcpy(maiorPalavra, palavraLida);
+            }
+            if(qntCarac < tamMenorPalavra && qntCarac > 0)
+            {
+                menorPalavra[0]='\0';
+                tamMenorPalavra = qntCarac;
+                strcpy(menorPalavra, palavraLida);
+            }
+            if(qntCarac > 0)
+                qntPalavras++;
+            qntCarac = 0;
+        }
+    }
+    return qntPalavras;
+}
+
 int main()
 {
-    int qntArq = 0;
+    int qntArq = 0, qntPalavras = 0;
     FILE *arquivo;
+    char MenorPalavraGlobal[255], MaiorPalavraGlobal[255];
 
     do
     {
     qntArq++;
-    char nomeArquivo[100];
+    char nomeArquivo[100], MaiorPalavraLocal[255], MenorPalavraLocal[255];
     GerarNomeArquivo(qntArq, nomeArquivo);
-    printf("%s\n",nomeArquivo);
     arquivo = fopen(nomeArquivo, "rb");
     if(arquivo == NULL)
         {
-            printf(" INFO - Foram lidos %d arquvios", qntArq - 1);
+            printf("INFO - Foram lidos %d arquvios", qntArq - 1);
             break;
         }
-    free(nomeArquivo);
-    }while (1 || qntArq >100);
+    qntPalavras += LeturaArquivo(arquivo, MaiorPalavraLocal, MenorPalavraLocal);
+    printf("INFO - Maior palavra do arquivo %d: %s\n", qntArq, MaiorPalavraLocal);
+    printf("INFO - Menor palavra do arquivo %d: %s\n\n", qntArq, MenorPalavraLocal);
+    }while (1 || qntArq > 1);
+
+    printf("Qnt palavras lidas: %d", qntPalavras);
     return 0;
 }
